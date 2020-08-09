6cc131d030b0ad38ca8ba77af3132b112cc60348
root <you@example.com>
2020-08-09 18:16:11 +0800

    -

--- superman.js
+++ superman.js
@@ -4,3 +4,4 @@ Work hard and earn money
 Work hard and earn money
 Work hard and earn money
 Work hard and earn money
+It will be better if you do not peek my committion. You should work hard and earn money
